7b7299bfe968351e13096ed83883af205f558da1
antek <antonimalinowski.kontakt@gmail.com>
2022-01-28 11:48:17 +0100

    modify custom component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 ﻿function ExpenseItem() {
-    return <h2>Expense Item!</h2>
+  return (
+    <div>
+      <div>December 13th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <p>$278.97</p>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
