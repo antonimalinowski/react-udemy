5136fbea2835cf8352a9b1c53696629792396bf7
antek <antonimalinowski.kontakt@gmail.com>
2022-02-04 12:09:24 +0100

    back to JSX syntax

--- src/App.js
+++ src/App.js
@@ -24,19 +24,19 @@ function App(props) {
     },
   ];
 
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h2", {}, "Let's get started!"),
-    React.createElement(Expenses, { items: expenses })
-  );
-
-  // return (
-  //   <div>
-  //     <h2>Let's get started!</h2>
-  //     <Expenses items={expenses} />
-  //   </div>
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h2", {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
   // );
+
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
+    </div>
+  );
 }
 
 export default App;
